9957318b308f32af428370542de8a2af2d9b79d9
featherbear <featherbear@users.noreply.github.com>
2020-08-05 18:53:18 +0000

    Automated deployment: Wed Aug  5 18:53:18 UTC 2020 71259ca606099b653bc00cef84f12ab487e1aed0

--- post/project/captures/analyser/grab.py
+++ post/project/captures/analyser/grab.py
@@ -2,104 +2,288 @@
 
 # z5206677
 
+from scipy.optimize import curve_fit
+import math
 import pandas as pd
 import numpy as np
-import glob, os
+import glob
+import os
+from pykalman import KalmanFilter
+import matplotlib.pyplot as plt
 
 data = dict()
 
 print("Reading data")
-os.chdir("../outdoor") # outdoor / indoor
 fileSuffix = ".pcapng.csv"
 
+oldCWD = os.getcwd()
+os.chdir("../outdoor")
+
 # Read csv files
+t = 0
 for file in glob.glob("*" + fileSuffix):
     point = int(file.replace(fileSuffix, ""))
     data[point] = pd.read_csv(file)
-    print(point, data[point]["wlan_radio.signal_dbm"].count() )
 
+    # print(point, data[point]["wlan_radio.signal_dbm"].count())
 # Order the data
 data = [(i, data[i]) for i in sorted(data.keys())]
 
 # Combine the data
-combined = pd.concat([pd.DataFrame(dict(RSS=v["wlan_radio.signal_dbm"],Distance=[i]*v["wlan_radio.signal_dbm"].count())) for (i,v) in data], ignore_index=True)
+combined = pd.concat(
+  [pd.DataFrame(dict(
+      RSS=v["wlan_radio.signal_dbm"],
+      Distance=[i]*v["wlan_radio.signal_dbm"].count()
+   )) for (i, v) in data
+  ],
+  ignore_index=True
+)
+
+X = combined.iloc[:, 1].values.reshape(-1, 1)
+Y = combined.iloc[:, 0].values.reshape(-1, 1)
 
 # Extract 10% of the data to be used as test data.
 # The other 90% is to be used as training data.
 testSample = combined.sample(frac=0.1)
 trainingSample = combined.drop(testSample.index)
 
+"""
+Analyse data and perform Kalman Filtering
+"""
+
+os.chdir(oldCWD)
+os.chdir("../../report/kalman_filter")
+
 # Analyse
 analysis = []
 for i, frame in data:
-  values = trainingSample.loc[trainingSample["Distance"] == i]
-  average = values["RSS"].mean()
-  analysis.append(dict(index=i, data=values, average=average))
+    values = trainingSample.loc[trainingSample["Distance"] == i]
 
-print("Averages:", *(map(lambda v: round(v,2), [data["average"] for data in analysis])))
+    average = values["RSS"].mean()
+    minimum = values["RSS"].min()
+    maximum = values["RSS"].max()
+    val_range = maximum - minimum
 
-import matplotlib.pyplot as plt
+    # Perform Kalman Filter algorithm
+    kf = KalmanFilter()
+    kalman_vals = kf.smooth(values["RSS"])[0]
 
-from sklearn.linear_model import LinearRegression
-X = combined.iloc[:, 1].values.reshape(-1, 1)
-Y = combined.iloc[:, 0].values.reshape(-1, 1)
+    # Generate Kalman filter overlay on top of raw values
+    assert(len(values["RSS"]) == len(kalman_vals))
+    plt.clf()
+    plt.title(f"RSS distribution at {i}m")
+    plt.xlabel('Samples')
+    plt.ylabel('RSS (dBm)')
+    plt.plot(range(len(kalman_vals)), values["RSS"], label="raw")
+    plt.plot(range(len(kalman_vals)), kalman_vals, label="filtered", alpha=0.9)
+    plt.legend()
+    plt.savefig(f"./{i}.png", dpi=300)
+    plt.clf()
 
-XYmodel = LinearRegression()  # create object for the class
-XYmodel.fit(X, Y)  # perform linear regression
-Y_pred = XYmodel.predict(X)  # make predictions
-print("\nXY R^2 score:", XYmodel.score(X, Y))
-print(f"XY: {round(XYmodel.coef_[0][0],3)}*distance + {XYmodel.intercept_[0]}")
-print(f"Manual YX: (RSS+{-round(XYmodel.intercept_[0],3)})/{round(XYmodel.coef_[0][0],3)}")
+    analysis.append(dict(
+        index=i,
+        data=values,
+        average=average,
+        range=val_range,
+        kalman=dict(
+            avg=(kalman_vals[-1][-1]+kalman_vals[-2][-1])/2,
+            data=kalman_vals,
+            range=kalman_vals.max() - kalman_vals.min()
+        )
+    ))
 
-'''
-YXmodel = LinearRegression()
-YXmodel.fit(Y, X)
-print("\nYX R^2 score:", YXmodel.score(Y, X))
-print(f"YX: {round(YXmodel.coef_[0][0],3)}*RSS + {YXmodel.intercept_[0]}")
-print()
 
 """
-Issues...
+Compare unfiltered vs kalman filter values
 """
-def plot_YX_regression(values):
-  X_pred = YXmodel.predict(values)
-  plt.scatter(X_pred, values, color="green", marker="x")
-  return X_pred
-'''
+print("Averages:", *(map(lambda v: round(v, 2), [data["average"] for data in analysis])))
+print("Kalman Averages:", *(map(lambda v: round(v, 2), [data["kalman"]["avg"] for data in analysis])))
 
-def plot_XY_regression():
-  plt.plot(X, Y_pred, color="red")
+print("RSS Range:", *(map(lambda v: round(v, 2), [data["range"] for data in analysis])))
+print("RSS Range (Kalman):", *(map(lambda v: round(v, 2), [data["kalman"]["range"] for data in analysis])))
 
-def plot_YX_manual(values):
-  formula = lambda rss: (rss-XYmodel.intercept_[0])/XYmodel.coef_[0][0]
-  x_values = [formula(v) for v in values]
-  plt.scatter(x_values, values, color="green", marker="x")
-  return x_values
+plt.clf()
+plt.title("Average Value vs Kalman Filter")
+plt.xlabel('Distance (m)')
+plt.ylabel('RSS (dBm)')
+xdata = np.array([data["index"] for data in analysis])
+plt.scatter(xdata, [data["average"] for data in analysis], label="average value")
+plt.scatter(xdata, [data["kalman"]["avg"] for data in analysis], label="kalman weighted value")
+plt.legend()
+plt.savefig("./average_vs_kalman_linear.png", dpi=300)
+plt.xscale('log')
+plt.xlim(right=15)
+plt.savefig("./average_vs_kalman_logarithmic.png", dpi=300)
+plt.clf()
+
+"""
+Data set removal
+"""
+
+os.chdir(oldCWD)
+os.chdir("../../report/set_removal")
+
+# Ignore distance 3 (outlier)
+strippedData = [*filter(lambda d: d["index"] != 3, analysis)]
+plt.clf()
+plt.title("Average Value vs Kalman Filter | 3m Redacted")
+plt.xlabel('Distance (m)')
+plt.ylabel('RSS (dBm)')
+xdata = indexData = np.array([data["index"] for data in strippedData])
+plt.scatter(xdata, [data["average"] for data in strippedData], label="average value")
+plt.scatter(xdata, [data["kalman"]["avg"] for data in strippedData], label="kalman weighted value")
+plt.legend()
+plt.savefig("./average_vs_kalman_no3_linear.png", dpi=300)
+plt.xscale('log')
+plt.xlim(right=15)
+plt.savefig("./average_vs_kalman_no3_logarithmic.png", dpi=300)
+plt.clf()
+
+"""
+Analyse other values
+"""
+
+os.chdir(oldCWD)
+os.chdir("../../report")
+
+plt.clf()
+plt.title("Signal Strength over Distance")
+plt.xlabel('Distance (m)')
+plt.ylabel('RSS (dBm)')
+plt.scatter(indexData, [data["kalman"]["avg"] for data in strippedData], label="kalman weighted value", color="#FF7900")
+plt.legend()
+plt.savefig("./kalman_no3_linear.png", dpi=300)
+plt.xscale('log')
+plt.xlim(right=15)
+plt.savefig("./kalman_no3_logarithmic.png", dpi=300)
+
+plt.clf()
+plt.title("RSS to Distance Distribution")
+plt.xlabel('RSS (dBm)')
+plt.ylabel('Distance (m)')
+plt.scatter(Y, X, label="recorded values")
+plt.scatter([data["kalman"]["avg"] for data in strippedData], indexData, label="kalman weighted value")
+plt.legend()
+plt.gca().invert_xaxis()
+plt.savefig("./dist_linear.png", dpi=300)
+plt.yscale('log')
+plt.ylim(top=15)
+plt.savefig("./dist_logarithmic.png", dpi=300)
+plt.clf()
+
+# Get RSS value at distance = 1m
+rss_1 = [data["kalman"]["avg"] for data in strippedData if data["index"] == 1][0]
+
+"""
+Calculate distance model parameters
+
+# RSS = loss at 1m - n10log(d) + chi
+# log(d) = (loss at 1m + chi (+) RSS)/10n
+# d = exp((loss at 1m + chi (+) RSS)/10n)
+
+:: Model: distance = exp( (rss_1 + RSS + chi) / 10n )
+"""
+def formula(x, n, chi):
+    val = rss_1 - x
+    val /= 10 * n
+    val = math.exp(val)
+    return val
+
+# Multi-function
+def fit_func(x, n, chi):
+    return [formula(X, n, chi) for X in x]
 
 
-def plot_average():
-  indexes = []; averages = []
-  for data in analysis:
-    indexes.append(data["index"])
-    averages.append(data["average"])
-  plt.scatter(indexes, averages, color="red", marker="s")
+xdata = [data["kalman"]["avg"] for data in strippedData]
+ydata = indexData
+(n, chi), pcov = curve_fit(fit_func, xdata, ydata)
 
-# plt.scatter(combined["Distance"], combined["RSS"])
+# https://stackoverflow.com/questions/19189362/getting-the-r-squared-value-using-curve-fit
+residuals = ydata - fit_func(xdata, n, chi)
+ss_res = np.sum(residuals**2)
+ss_tot = np.sum((ydata-np.mean(ydata))**2)
+r_squared = 1 - (ss_res / ss_tot)
 
-plt.scatter(X,Y) # Actual values
-# plot_XY_regression()
-plot_average()
+print(f"distance = exp( ({round(rss_1, 2)} + RSS + {round(chi,2)}) / (10*{round(n,2)}) )")
+print(f"R^2 = {round(r_squared, 2)}")
+
+"""
+Display model fit
+"""
+plt.clf()
+plt.title("RSS to Distance Distribution")
+plt.xlabel('RSS (dBm)')
+plt.ylabel('Distance (m)')
+plt.scatter(Y, X, label="recorded values")
+xdata = [data["kalman"]["avg"] for data in analysis]
+ydata = [data["index"] for data in analysis]
+plt.scatter(xdata, ydata, label="kalman weighted value")
+xdata = [*range(-80, 0)]
+ydata = [formula(i, n, chi) for i in range(-80, 0)]
+plt.plot(xdata, ydata, label=f"line (n: {round(n,2)}, $\chi$: {round(chi,2)})", color="green")
+plt.legend()
+plt.gca().invert_xaxis()
+plt.ylim(top=15)
+plt.savefig("./fit_linear.png", dpi=300)
+plt.yscale('log')
+plt.autoscale()
+plt.xlim(left=rss_1 // 4 * 3)
+plt.ylim(bottom=0.5, top=15)
+plt.savefig("./fit_logarithmic.png", dpi=300)
+plt.clf()
+
+"""
+Check model accuracy
+"""
 
 print()
+plt.clf()
+plt.title("RSS to Distance Test")
+plt.xlabel('RSS (dBm)')
+plt.ylabel('Distance (m)')
+plt.gca().invert_xaxis()
+
+plt.scatter([], [], label="hit", color="green")
+plt.scatter([], [], label="near", color="lightgreen")
+plt.scatter([], [], label="miss", color="grey")
+plt.legend()
+
+total_hit = 0
+total_near = 0
+total = 0
+
 for data in analysis:
-  # plot_YX_regression(data["testSample"].values.reshape(-1, 1))
   correctDistance = data["index"]
   samples = testSample.loc[testSample["Distance"] == correctDistance]["RSS"]
-  x_values = plot_YX_manual(samples)
-  count = sum(map(lambda v: (correctDistance-0.5 <= v <= correctDistance+0.5) and 1 or 0, x_values))
-  print(f"Hit-rate for {correctDistance}m :: {count}/{len(x_values)} :: {round(count/len(x_values)*100,2)}%")
+  
+  approximations = fit_func(samples, n, chi)
+  
+  tolerance = 1
+  nearTolerance = 1.5
 
-#plt.xscale('log')
-plt.xlabel("Distance (m)")
-plt.ylabel("RSS (dBm)")
+  hit   = []
+  near  = []
+  miss  = []
+  
+  for i in range(len(approximations)):
+    if correctDistance - tolerance <= approximations[i] <= correctDistance + tolerance:
+      hit.append(samples.iloc[i])
+    elif correctDistance - tolerance - nearTolerance <= approximations[i] <= correctDistance + tolerance + nearTolerance:
+      near.append(samples.iloc[i])
+    else:
+      miss.append(samples.iloc[i])
+  
+  total_hit += len(hit)
+  total_near += len(near)
+  total += len(approximations)
+
+  plt.scatter(hit, [correctDistance] * len(hit), label="hit", color="green")
+  plt.scatter(near, [correctDistance] * len(near), label="near", color="lightgreen")
+  plt.scatter(miss, [correctDistance] * len(miss), label="miss", color="grey")
+
+  print(f"Hit-rate for {correctDistance}m :: {len(hit)+len(near)}/{len(approximations)} :: {round((len(hit)+len(near))/len(approximations)*100,2)}%")
+
+print(f"\nTotal hit-rate :: {total_hit+total_near}/{total} :: {round((total_hit+total_near)/total*100,2)}%")
+
+plt.savefig(f"./fit_test.png", dpi=300)
 plt.show()
